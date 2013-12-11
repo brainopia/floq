9ade0177520a7cd4d9165c9d5e0e8bb9e78449a6
brainopia <brainopia@evilmartians.com>
2013-12-12 01:23:16 +0400

    Spec more complex behavior of test scheduler

--- floq/spec/floq/scheduler_spec.rb
+++ floq/spec/floq/scheduler_spec.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 
 describe Floq::Schedulers::Test do
   let(:mail) { Hash.new {|hash,key| hash[key] = [] }}
-  let(:queues) { queues_for queue_names }
+  let(:queues) { queues_for *queue_names }
+  let(:queue_names) { [:queue_1, :queue_2] }
   subject { described_class.new queues }
 
-  before { subject }
+  before { subject.drop }
 
   def queues_for(*names)
     names.map do |name|
@@ -15,19 +16,22 @@ describe Floq::Schedulers::Test do
     end
   end
 
-  context 'one queue' do
-    let(:queue_names) { :queue }
+  its(:pushed) { [] }
 
-    its(:pushed) { [] }
+  it 'should run without messages' do
+    expect { subject.run }.not_to raise_exception
+  end
 
-    it 'should run without messages' do
-      expect { subject.run }.not_to raise_exception
-    end
+  it 'should handle a simple message' do
+    Floq[:queue_1].push :a_message
+    subject.run
+    mail.should == { queue_1: [ :a_message ] }
+  end
 
-    it 'should handle simple message' do
-      Floq[:queue].push :a_message
-      subject.run
-      mail.should == { queue: [ :a_message ] }
-    end
+  it 'should handle nested messages' do
+    Floq[:queue_1].handle {|data| Floq[:queue_2].push data }
+    Floq[:queue_1].push :routed_message
+    subject.run
+    mail.should == { queue_2: [ :routed_message ] }
   end
 end
