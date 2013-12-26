0d328e2f432cfb554ac7f5a3f26138275511f0ec
brainopia <brainopia@evilmartians.com>
2013-12-26 16:55:21 +0400

    Fix rails application root and remove extra dependencies

--- floq-web/lib/floq/web/application.rb
+++ floq-web/lib/floq/web/application.rb
@@ -1,12 +1,13 @@
 require_relative '../web'
-require 'action_controller'
-require 'rails/all'
+require 'action_controller/railtie'
+require 'action_view/railtie'
 
 class Floq::Web::Application < Rails::Application
   routes.append do
     mount Floq::Web => '/'
   end
 
+  config.root = File.expand_path '../../..', __dir__
   config.eager_load = true
   config.secret_key_base = '404'
 end
