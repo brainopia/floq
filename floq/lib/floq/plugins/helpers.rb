88f292ff903a0d23d121549b8eb2b4f9e52b0737
brainopia <brainopia@evilmartians.com>
2014-01-24 14:48:54 +0400

    Raise if pull without block

--- floq/lib/floq/plugins/helpers.rb
+++ floq/lib/floq/plugins/helpers.rb
@@ -25,4 +25,12 @@ class Floq::Plugins::Helpers
   def pull_and_handle(queue)
     @adapter.pull(queue) {|data| handler(queue).call data }
   end
+
+  def pull(queue, &block)
+    raise ArgumentError, <<-ERROR unless block
+      pull from #{queue} without a block
+    ERROR
+
+    @adapter.pull queue, &block
+  end
 end
