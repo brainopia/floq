c6475ad288e99df382be31573cbc938ea105f38d
brainopia <brainopia@evilmartians.com>
2014-01-27 09:22:26 +0400

    Load greedy scheduler

--- floq/lib/floq/schedulers.rb
+++ floq/lib/floq/schedulers.rb
@@ -2,4 +2,5 @@ module Floq::Schedulers
   require_relative 'schedulers/base'
   require_relative 'schedulers/test'
   require_relative 'schedulers/periodic'
+  require_relative 'schedulers/greedy'
 end
