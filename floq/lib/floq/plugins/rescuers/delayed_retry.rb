037bc996a2edb2bd396f4ca651804adc59e1e99d
brainopia <brainopia@evilmartians.com>
2014-02-06 02:45:27 +0400

    Fix delayed_retry multithread issue

--- floq/lib/floq/plugins/rescuers/delayed_retry.rb
+++ floq/lib/floq/plugins/rescuers/delayed_retry.rb
@@ -4,31 +4,32 @@ class Floq::Plugins::Rescuers::DelayedRetry
 
   def initialize(adapter, callback=nil)
     super
-    @last_messages = {}
+    @messages = {}
+    @retries = {}
     @callback = callback
   end
 
   def pull(queue, &block)
-    if @delayed_retry
-      if Time.now - @delayed_retry <= FAIL_TIMEOUT
+    if @retries[queue]
+      if Time.now - @retries[queue] <= FAIL_TIMEOUT
         return
       else
-        @delayed_retry = nil
+        @retries[queue] = nil
         try_again = true
       end
     end
 
     begin
       if try_again
-        block.call @last_messages[queue]
+        block.call @messages[queue]
       else
         @adapter.pull queue do |message|
-          @last_messages[queue] = message
+          @messages[queue] = message
           block.call message
         end
       end
     rescue
-      @delayed_retry = Time.now
+      @retries[queue] = Time.now
       @callback.call queue, $! if @callback
     end
   end
