74eefdd4fd825535f1565c0a19db7aa398f19439
brainopia <brainopia@evilmartians.com>
2014-03-24 15:22:04 +0400

    Use evalsha instead of eval in redis adapter

--- floq-redis/lib/floq/plugins/adapters/redis.rb
+++ floq-redis/lib/floq/plugins/adapters/redis.rb
@@ -3,6 +3,7 @@ class Floq::Plugins::Adapters::Redis
 
   def initialize(settings={})
     @pool = new_pool settings.dup
+    @digests = {}
   end
 
   def peek(queue)
@@ -44,16 +45,14 @@ class Floq::Plugins::Adapters::Redis
   end
 
   def offset(queue)
-    pool.with do |client|
-      client.eval <<-LUA, argv: [offset_base_key(queue), offset_key(queue)]
-        local base_key   = table.remove(ARGV, 1)
-        local offset_key = table.remove(ARGV, 1)
-        local base       = redis.call('get', base_key) or 0
-        local offset     = redis.call('get', offset_key) or 0
+    redis_eval <<-LUA, [offset_base_key(queue), offset_key(queue)]
+      local base_key   = table.remove(ARGV, 1)
+      local offset_key = table.remove(ARGV, 1)
+      local base       = redis.call('get', base_key) or 0
+      local offset     = redis.call('get', offset_key) or 0
 
-        return tonumber(base) + tonumber(offset)
-      LUA
-    end
+      return tonumber(base) + tonumber(offset)
+    LUA
   end
 
   def dirty_offset(queue)
@@ -63,33 +62,29 @@ class Floq::Plugins::Adapters::Redis
   end
 
   def offset!(queue, value)
-    pool.with do |client|
-      client.eval <<-LUA, argv: [offset_base_key(queue), offset_key(queue), value]
-        local base_key   = table.remove(ARGV, 1)
-        local offset_key = table.remove(ARGV, 1)
-        local value      = tonumber(table.remove(ARGV, 1))
-        local base       = tonumber(redis.call('get', base_key) or 0)
+    redis_eval <<-LUA, [offset_base_key(queue), offset_key(queue), value]
+      local base_key   = table.remove(ARGV, 1)
+      local offset_key = table.remove(ARGV, 1)
+      local value      = tonumber(table.remove(ARGV, 1))
+      local base       = tonumber(redis.call('get', base_key) or 0)
 
-        if value < base then
-          return { err = "Value is less than offset base" }
-        else
-          redis.call('set', offset_key, value - base)
-        end
-      LUA
-    end
+      if value < base then
+        return { err = "Value is less than offset base" }
+      else
+        redis.call('set', offset_key, value - base)
+      end
+    LUA
   end
 
   def total(queue)
-    pool.with do |client|
-      client.eval <<-LUA, argv: [queue, offset_base_key(queue)]
-        local queue    = table.remove(ARGV, 1)
-        local base_key = table.remove(ARGV, 1)
-        local tasks    = redis.call('llen', queue)
-        local base     = redis.call('get', base_key) or 0
+    redis_eval <<-LUA, [queue, offset_base_key(queue)]
+      local queue    = table.remove(ARGV, 1)
+      local base_key = table.remove(ARGV, 1)
+      local tasks    = redis.call('llen', queue)
+      local base     = redis.call('get', base_key) or 0
 
-        return tasks + tonumber(base)
-      LUA
-    end
+      return tasks + tonumber(base)
+    LUA
   end
 
   def dirty_total(queue)
@@ -99,84 +94,76 @@ class Floq::Plugins::Adapters::Redis
   end
 
   def peek_and_skip(queue)
-    pool.with do |client|
-      # TODO: evalsha
-      client.eval <<-LUA, argv: [queue.to_s, offset_key(queue), offset_base_key(queue)]
-        local queue      = table.remove(ARGV, 1)
-        local offset_key = table.remove(ARGV, 1)
-        local base_key   = table.remove(ARGV, 1)
-        local offset     = redis.call('get', offset_key) or 0
-        local message    = redis.call('lindex', queue, offset)
+    redis_eval <<-LUA, [queue.to_s, offset_key(queue), offset_base_key(queue)]
+      local queue      = table.remove(ARGV, 1)
+      local offset_key = table.remove(ARGV, 1)
+      local base_key   = table.remove(ARGV, 1)
+      local offset     = redis.call('get', offset_key) or 0
+      local message    = redis.call('lindex', queue, offset)
 
-        if message then
-          local base = redis.call('get', base_key) or 0
-          redis.call('incr', offset_key)
-          return { message, tonumber(base) + tonumber(offset) }
-        end
-      LUA
-    end
+      if message then
+        local base = redis.call('get', base_key) or 0
+        redis.call('incr', offset_key)
+        return { message, tonumber(base) + tonumber(offset) }
+      end
+    LUA
   end
 
   def recover(queue)
-    pool.with do |client|
-      # TODO: evalsha
-      # fix this awful code
-      client.eval <<-LUA, argv: [queue.to_s, offset_base_key(queue), recover_offset_key(queue), confirm_key(queue)]
-        local queue       = table.remove(ARGV, 1)
-        local base_key    = table.remove(ARGV, 1)
-        local recover_key = table.remove(ARGV, 1)
-        local confirm_key = table.remove(ARGV, 1)
-        local confirms    = redis.call('get', confirm_key)
+    # fix this awful code
+    redis_eval <<-LUA, [queue.to_s, offset_base_key(queue), recover_offset_key(queue), confirm_key(queue)]
+      local queue       = table.remove(ARGV, 1)
+      local base_key    = table.remove(ARGV, 1)
+      local recover_key = table.remove(ARGV, 1)
+      local confirm_key = table.remove(ARGV, 1)
+      local confirms    = redis.call('get', confirm_key)
 
-        if confirms then
-          local recover_offset = redis.call('get', recover_key) or 0
-          local recover_byte   = math.floor(recover_offset / 8)
-          local recover_bit    = recover_offset - recover_byte * 8
-          local position
-          local byte
+      if confirms then
+        local recover_offset = redis.call('get', recover_key) or 0
+        local recover_byte   = math.floor(recover_offset / 8)
+        local recover_bit    = recover_offset - recover_byte * 8
+        local position
+        local byte
 
-          for byte_index=recover_byte, #confirms - 1 do
-            byte = string.byte(confirms, byte_index + 1)
+        for byte_index=recover_byte, #confirms - 1 do
+          byte = string.byte(confirms, byte_index + 1)
 
-            for bit_index=recover_bit, 7 do
-              if byte % 2^(7-bit_index+1) < 2^(7-bit_index) then
-                position = byte_index*8 + bit_index
-                break
-              end
-            end
-
-            if position then
+          for bit_index=recover_bit, 7 do
+            if byte % 2^(7-bit_index+1) < 2^(7-bit_index) then
+              position = byte_index*8 + bit_index
               break
             end
-
-            recover_bit = 0
           end
 
           if position then
-            local message = redis.call('lindex', queue, position)
-            local base = redis.call('get', base_key) or 0
-
-            redis.call('set', recover_key, position + 1)
-            return { message, tonumber(base) + tonumber(position) }
+            break
           end
+
+          recover_bit = 0
         end
-      LUA
-    end
+
+        if position then
+          local message = redis.call('lindex', queue, position)
+          local base = redis.call('get', base_key) or 0
+
+          redis.call('set', recover_key, position + 1)
+          return { message, tonumber(base) + tonumber(position) }
+        end
+      end
+    LUA
   end
 
   def confirm(queue, offset)
-    pool.with do |client|
-      client.eval <<-LUA, argv: [confirm_key(queue), offset_base_key(queue), offset]
-        local confirm_key = table.remove(ARGV, 1)
-        local base_key    = table.remove(ARGV, 1)
-        local offset      = tonumber(table.remove(ARGV, 1))
-        local base        = tonumber(redis.call('get', base_key) or 0)
+    redis_eval <<-LUA, [confirm_key(queue), offset_base_key(queue), offset]
+      local confirm_key = table.remove(ARGV, 1)
+      local base_key    = table.remove(ARGV, 1)
+      local offset      = tonumber(table.remove(ARGV, 1))
+      local base        = tonumber(redis.call('get', base_key) or 0)
 
-        if base <= offset then
-          redis.call('setbit', confirm_key, offset - base, 1)
-        end
-      LUA
-    end
+      if base <= offset then
+        redis.call('setbit', confirm_key, offset - base, 1)
+      end
+    LUA
   end
 
   # TODO: take into account offset_base
@@ -189,56 +176,51 @@ class Floq::Plugins::Adapters::Redis
   def cleanup(queue, type=:default)
     case type
     when :singular
-      pool.with do |client|
-        client.eval <<-LUA, argv: [queue.to_s, offset_key(queue), offset_base_key(queue)]
-          local queue      = table.remove(ARGV, 1)
-          local offset_key = table.remove(ARGV, 1)
-          local base_key   = table.remove(ARGV, 1)
-          local offset     = redis.call('get', offset_key)
+      redis_eval <<-LUA, [queue.to_s, offset_key(queue), offset_base_key(queue)]
+        local queue      = table.remove(ARGV, 1)
+        local offset_key = table.remove(ARGV, 1)
+        local base_key   = table.remove(ARGV, 1)
+        local offset     = redis.call('get', offset_key)
 
-          if offset and offset ~= 0 then
-            redis.call('del', offset_key)
-            redis.call('incrby', base_key, offset)
-            redis.call('ltrim', queue, offset, -1)
-          end
-        LUA
-      end
+        if offset and offset ~= 0 then
+          redis.call('del', offset_key)
+          redis.call('incrby', base_key, offset)
+          redis.call('ltrim', queue, offset, -1)
+        end
+      LUA
     when :parallel
-      pool.with do |client|
-        # TODO: evalsha
-        # fix this awful code
-        client.eval <<-LUA, argv: [queue.to_s, offset_key(queue), offset_base_key(queue), recover_offset_key(queue), confirm_key(queue)]
-          local queue       = table.remove(ARGV, 1)
-          local offset_key  = table.remove(ARGV, 1)
-          local base_key    = table.remove(ARGV, 1)
-          local recover_key = table.remove(ARGV, 1)
-          local confirm_key = table.remove(ARGV, 1)
-          local offset      = redis.call('get', offset_key)
-          local confirms    = redis.call('get', confirm_key)
-          local cursor      = 1
+      # fix this awful code
+      redis_eval <<-LUA, [queue.to_s, offset_key(queue), offset_base_key(queue), recover_offset_key(queue), confirm_key(queue)]
+        local queue       = table.remove(ARGV, 1)
+        local offset_key  = table.remove(ARGV, 1)
+        local base_key    = table.remove(ARGV, 1)
+        local recover_key = table.remove(ARGV, 1)
+        local confirm_key = table.remove(ARGV, 1)
+        local offset      = redis.call('get', offset_key)
+        local confirms    = redis.call('get', confirm_key)
+        local cursor      = 1
 
-          if confirms then
-            while true do
-              if string.byte(confirms, cursor) == 255 then
-                cursor = cursor + 1
-              else
-                break
-              end
-            end
-
-            redis.call('set', recover_key, 0)
-
-            if cursor > 1 then
-              local deletedConfirmations = (cursor-1)*8
-
-              redis.call('incrby', base_key, deletedConfirmations)
-              redis.call('set', offset_key, offset - deletedConfirmations)
-              redis.call('set', confirm_key, string.sub(confirms, cursor, -1))
-              redis.call('ltrim', queue, deletedConfirmations, -1)
+        if confirms then
+          while true do
+            if string.byte(confirms, cursor) == 255 then
+              cursor = cursor + 1
+            else
+              break
             end
           end
-        LUA
-      end
+
+          redis.call('set', recover_key, 0)
+
+          if cursor > 1 then
+            local deletedConfirmations = (cursor-1)*8
+
+            redis.call('incrby', base_key, deletedConfirmations)
+            redis.call('set', offset_key, offset - deletedConfirmations)
+            redis.call('set', confirm_key, string.sub(confirms, cursor, -1))
+            redis.call('ltrim', queue, deletedConfirmations, -1)
+          end
+        end
+      LUA
     end
   end
 
@@ -267,6 +249,18 @@ class Floq::Plugins::Adapters::Redis
 
     ConnectionPool.new pool_settings do
       Redis.new settings
+    end
+  end
+
+  def redis_eval(script, args)
+    pool.with do |client|
+      begin
+        @digests[script] ||= Digest::SHA1.hexdigest script
+        client.evalsha @digests[script], argv: args
+      rescue Redis::CommandError => error
+        raise unless error.message.include? 'NOSCRIPT'
+        client.eval script, argv: args
+      end
     end
   end
 end
